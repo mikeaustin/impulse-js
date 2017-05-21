496847e914049ab82d4ca5711d2fa5331838a3b3
Mike Austin <mike_ekim@yahoo.com>
2017-05-20 18:29:11 -0700

    Change first() to slice()

--- experiments/functional.im.js
+++ experiments/functional.im.js
@@ -6,29 +6,23 @@ extend Array {
 }
 
 extend String {
-  function first(n) {
-    return this.slice(0, 2);
+  function slice(begin, end) {
+    return this.slice(begin, end);
   }
 }
 
 var firstTwoLetters = (words) => {
-  return words.map(word => word.first(2));
+  return words.map(word => word.slice(0, 2));
 };
 
 console.log(firstTwoLetters(["jim", "kate"]));
 
-var firstTwo = _methods.first(2);
 
-console.log(">>>", firstTwo("jim"));
+var _ = _methods;
 
-
-//console.log(_methods.map.apply(["jim", "kate"], [word => _methods.first.apply(word, [2])]));
-
-// var firstTwoLetters2 = _methods.map(_methods.first(2));
-// var firstTwoLetters2 = _methods.map.bindArgs(_methods.first.bindArgs(2));
-var firstTwoLetters2 = _methods.map(_methods.first(2));
-
-// var firstTwoLetters2 = _.map(_.first(2));
+var firstTwoLetters2 = _.map(_.slice(0, 2));
 
 console.log(firstTwoLetters2(["jim", "kate"]));
+
+console.log(_.map(_.slice(0, 2))(["jim", "kate"]));
 
