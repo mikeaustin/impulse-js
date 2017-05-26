37002d162c724a8a5b18e01194a49f8218f0f4e2
Mike Austin <mike_ekim@yahoo.com>
2017-05-25 22:11:11 -0700

    Add null Literal; Update README with examples; Change require path to absolute (symlink in node_modules)

--- test/impulse/include.im.js
+++ test/impulse/include.im.js
@@ -1,2 +1,53 @@
-console.log("[ainclude.im.js]");
+
+trait Aggregate (reduce) {
+
+  function max() {
+    return Math.max.apply(null, this);
+  }
+
+}
+
+extend Array with Aggregate {
+
+  function filter(func) {
+    return this.filter(func);
+  }
+
+}
+
+console.log("[1, 2, 3].max() ==", [1, 3, 2].max());
+
+
+//
+
+
+extend Number {
+
+  function isEven() { return this % 2 == 0; }
+  function isOdd() { return !(this.isEven()); }
+
+}
+
+var odds = _.filter(_.isOdd());
+
+console.log("odds([1, 2, 3]) ==", odds([1, 2, 3]));
+
+
+//
+
+
+class Point extends Object {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  function _add(that) {
+    return new Point(this.x + that.x, this.y + that.y);
+  }
+
+}
+
+console.log("new Point(2, 3) + new Point(2, 3) ==", new Point(2, 3) + new Point(2, 3));
 
